6b553be87cfd980639ba1c0386179e45d4177980
David Boslee <david@goteleport.com>
2024-08-29 08:22:26 -0600

    Fix deadlock in identity watcher
    A backend disconnect will cause the receiver of the watcher events
    to exit. If this occurs while and the sender has enough events
    to fill the channel buffer deadlock can occur.
    
    Signed-off-by: David Boslee <david@goteleport.com>

--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -814,13 +814,13 @@ reList:
 				scopedLog.Debugf("Emitting list result as %s event for %s=%s", t, key.Key, key.Value)
 			}
 
-			queueStart := spanstat.Start()
-			w.Events <- KeyValueEvent{
+			if ok := w.emit(ctx, KeyValueEvent{
 				Key:   string(key.Key),
 				Value: key.Value,
 				Typ:   t,
+			}); !ok {
+				return
 			}
-			trackEventQueued(string(key.Key), t, queueStart.End(true).Total())
 		}
 
 		nextRev := revision + 1
@@ -837,15 +837,16 @@ reList:
 			if traceEnabled {
 				scopedLog.Debugf("Emitting EventTypeDelete event for %s", k)
 			}
-
-			queueStart := spanstat.Start()
-			w.Events <- event
-			trackEventQueued(k, EventTypeDelete, queueStart.End(true).Total())
+			if ok := w.emit(ctx, event); !ok {
+				return
+			}
 		})
 
 		// Only send the list signal once
 		if !listSignalSent {
-			w.Events <- KeyValueEvent{Typ: EventTypeListDone}
+			if ok := w.emit(ctx, KeyValueEvent{Typ: EventTypeListDone}); !ok {
+				return
+			}
 			listSignalSent = true
 		}
 
@@ -927,10 +928,9 @@ reList:
 					if traceEnabled {
 						scopedLog.Debugf("Emitting %s event for %s=%s", event.Typ, event.Key, event.Value)
 					}
-
-					queueStart := spanstat.Start()
-					w.Events <- event
-					trackEventQueued(string(ev.Kv.Key), event.Typ, queueStart.End(true).Total())
+					if ok := w.emit(ctx, event); !ok {
+						return
+					}
 				}
 			}
 		}
